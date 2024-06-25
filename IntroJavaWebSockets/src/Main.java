63b75047852988e4422564923a40f7588e0172cd
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-06-25 22:03:21 +0300

    Commit message >>>>> websockt programming <<<<<

--- IntroJavaWebSockets/src/Main.java
+++ IntroJavaWebSockets/src/Main.java
@@ -1,7 +1,16 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.http.WebSocket;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8080);
+
+
+        Socket soc = serverSocket.accept();
 
     }
 }
