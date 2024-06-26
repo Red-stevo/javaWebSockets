00937dd2ccbc2a62b30ce7d5b294d47b72bee1ab
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-06-26 07:53:10 +0300

    Commit message >>>>> done <<<<<

--- IntroJavaWebSockets/src/Main.java
+++ IntroJavaWebSockets/src/Main.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.http.WebSocket;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -11,12 +10,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(8080);
 
-
         Socket soc = serverSocket.accept();
-
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(soc.getInputStream()));
 
         String str = reader.readLine();
+
+        System.out.println(str);
     }
 }
