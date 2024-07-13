8022a1c1f78a3d75428626b391eb63fdbabc93b9
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-07-13 13:32:06 +0300

    Commit message >>>>> spring web project initialization <<<<<

---
+++ websocketsProject/src/main/java/org/red/stevo/com/websocketsproject/WebsocketsProjectApplication.java
@@ -0,0 +1,13 @@
+package org.red.stevo.com.websocketsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketsProjectApplication.class, args);
+	}
+
+}
---
+++ websocketsProject/src/test/java/org/red/stevo/com/websocketsproject/WebsocketsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.red.stevo.com.websocketsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
