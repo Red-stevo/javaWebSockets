4e8b9de37e47209595482915f762898c03e0d667
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-07-13 13:27:10 +0300

    Commit message >>>>> web sockets working <<<<<

--- WebSocketsClient/src/Main.java
+++ WebSocketsClient/src/Main.java
@@ -3,19 +3,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Arrays;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("192.168.0.125", 8080);
+    public static void main(String[] args){
+        try {
+            Socket socket = new Socket("192.168.0.125", 8080);
 
-        PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+            PrintWriter printWriter = null;
 
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Data : ");
-        String data = bufferedReader.readLine();
+            printWriter = new PrintWriter(socket.getOutputStream(), true);
 
-        printWriter.println(data);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Data : ");
+            String data = bufferedReader.readLine();
+
+            printWriter.println(data);
+        } catch (IOException e) {
+            System.out.println("Connection Refused.");
+        }catch (Exception e){
+            System.out.println(Arrays.toString(e.getStackTrace()));
+        }
+
+
     }
 }
